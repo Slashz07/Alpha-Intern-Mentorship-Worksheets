5e5611897e0cdee6dd86012d378a88886ce8fe92
Slashz07 <sumitforsaken76248@gmail.com>
2024-03-31 22:57:29 +0530

    Find the doublet in the Array whose sum is equal to the given value x

--- c++/14th worksheet/findingDoubletSum.cpp
+++ c++/14th worksheet/findingDoubletSum.cpp
@@ -1,17 +1,21 @@
-// Find the doublet in the Array whose sum is 
+// Find the doublet in the Array whose sum is
 // equal to the given value x
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
   vector<int> v = {2, 5, 3, 1, 8, 9, 12, 15, 21, 51};
   int sumNum;
   cout << "enter the sum whose doublet is to be found: ";
   cin >> sumNum;
-  for (int i = 0; i < v.size() - 1;i++){
-    for (int j = i + 1; j < v.size();j++){
-      if(v[i]+v[j]==sumNum){
+  for (int i = 0; i < v.size() - 1; i++)
+  {
+    for (int j = i + 1; j < v.size(); j++)
+    {
+      if (v[i] + v[j] == sumNum)
+      {
         cout << "doublet found in the array is: " << v[i] << " and " << v[j];
         exit(1);
       }
