34bc42dcb9d50878d3ee10880ee74f4bb106f8dc
Slashz07 <sumitforsaken76248@gmail.com>
2024-04-27 09:22:54 +0530

    string questions

---
+++ c++/17th worksheet/updateAllEvenStringPositions.cpp
@@ -0,0 +1,22 @@
+//  Input a string of size n and Update all
+// the even positions in the string to character
+// ‘a’. Consider 0-based indexing.
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+  string str;
+  cout << "Enter a string: ";
+  getline(cin, str);
+  for (int i = 0; i < str.length(); i++)
+    if (i % 2 == 0)
+    {
+      str[i] = 'a';
+    }
+  }
+  cout << str;
+  return 0;
+}
