c6362ad3227aa5ca06a031cac9283c99bc9df97f
Slashz07 <sumitforsaken76248@gmail.com>
2024-04-27 12:06:12 +0530

    Given two strings s and t, return true if t is an anagram of s, and false otherwise.

--- c++/17th worksheet/checkForAnagram.cpp
+++ c++/17th worksheet/checkForAnagram.cpp
@@ -6,26 +6,18 @@ using namespace std;
 
 int main()
 {
-  string str1 = "William Shakespeare";
-  string str2 = "I'll make a wise phrase";
+  string str1 = "Clint Eastwood";
+  string str2 = "Old West Action";
   int arr1[26] = {0};
   int arr2[26] = {0};
   for (int i = 0; i < str1.length(); i++)
   {
     arr1[tolower(str1[i]) - 'a']++;
   }
-  for (int i = 0; i < 26; i++)
-  {
-    cout << arr1[i] << ' ';
-  }
-  cout << endl;
+
   for (int i = 0; i < str2.length(); i++)
   {
     arr2[tolower(str2[i]) - 'a']++;
-  }
-  for (int i = 0; i < 26; i++)
-  {
-    cout << arr2[i] << ' ';
   }
 
   for (int i = 0; i < 26; i++)
